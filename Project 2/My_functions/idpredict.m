2d4b1054555add7e093ab68471dcdc0a2e4a5006
AronssonAlfred <152867194+AronssonAlfred@users.noreply.github.com>
2024-05-02 15:45:59 +0200

    trial update

--- Project 2/My_functions/idpredict.m
+++ Project 2/My_functions/idpredict.m
@@ -9,9 +9,6 @@ function y_pred = idpredict(model, z, horizon)
 
         phi(:,1) = []; % Why the fuck is that even there?
 
-        %y_pred(end-length(phi)+1:end) = sum(model.theta(model.na+1:end)'.*phi(:,model.na+2:end),2) + ... % b0*u(t) + b1*u(t-1) ... 
-            %sum(model.theta(2:model.na+1)'.*phi(:,2:model.na+1),2); % a1*y(t-1) + a2*y(t-2) ... 
-
          y_pred(end-length(phi)+1:end) = phi*model.theta;
         
         z(:,1) = y_pred; % Prediction as base for future predictions
--- Project 2/student_test_pr2.m
+++ Project 2/student_test_pr2.m
@@ -45,7 +45,7 @@ model.theta;
 %    -0.5004
 %     0.2366
 
-idpredict(model,[y u],1)
+idpredict(model,[y u],1);
 
 %          0
 %     2.3146
@@ -55,7 +55,7 @@ idpredict(model,[y u],1)
 %    -0.5496
 %     1.7105
 
-idpredict(model,[y u],3)
+idpredict(model,[y u],3);
 
 %          0
 %          0
@@ -65,7 +65,7 @@ idpredict(model,[y u],3)
 %    -0.2234
 %     0.5249
 
-%ysim=idsimulate(model,u);
+ysim=idsimulate(model,u)
 
 %          0
 %          0
