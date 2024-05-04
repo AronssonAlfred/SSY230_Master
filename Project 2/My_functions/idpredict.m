f81f257228a6c7a0fbf60330ccf2af5f50911165
AronssonAlfred <152867194+AronssonAlfred@users.noreply.github.com>
2024-05-04 10:45:50 +0200

    Some changes to idpredict

--- Project 2/My_functions/idpredict.m
+++ Project 2/My_functions/idpredict.m
@@ -1,20 +1,14 @@
 function y_pred = idpredict(model, z, horizon)
+    zero = max(model.na, model.nb + model.nk - 1);
+    phi = uy2phi([zeros(zero, 2); z], [model.na, model.nb, model.nk]); % Generate regressor
+    phi(:, 1) = []; % Remove the first column (if needed, depending on your uy2phi implementation)
+    y_pred = phi * model.theta; % Initial prediction
 
-    zero = max(model.na, model.nb+model.nk-1);
-    z = [zeros(zero,2);z];
-    
-
-    for k = 1:horizon
-
-        phi = uy2phi(z,[model.na, model.nb, model.nk]); % Generate Regressor
-        phi(:,1) = [];
-        y_pred = phi*model.theta;
-        z(:,1) = [zeros(zero,1); y_pred];
-
+    % If more steps are needed, recursively predict further
+    if horizon > 1
+        % Update input data for recursive prediction
+        % Assumes the model inputs are in the 2nd column of z and outputs are in the 1st column
+        new_input = [y_pred, z(:, 2)];
+        y_pred = idpredict(model, new_input, horizon-1);
     end
-
-
-
-    
 end
-
