09f296b4dec01606f4ccb6f2386697a675bc1aa6
AronssonAlfred <152867194+AronssonAlfred@users.noreply.github.com>
2024-05-06 18:28:33 +0200

    Further progress on idpredict

--- Project 2/My_functions/idpredict.m
+++ Project 2/My_functions/idpredict.m
@@ -2,18 +2,25 @@ function y_pred = idpredict(model, z, horizon)
 
     zero = max(model.na, model.nb + model.nk - 1);
     phi = uy2phi([zeros(zero, 2); z], [model.na, model.nb, model.nk]); % Generate regressor
-    phi(:, 1) = []; % Remove the first column (if needed, depending on your uy2phi implementation)
+    phi(:, 1) = []; % Remove the first column 
     y_pred = phi * model.theta; % Initial prediction
 
 
     for k = 2:horizon
         
-        zero = max(zero, k);
+        % Calculate y_hat(t|t-k)
         phi_temp = uy2phi([zeros(zero, 2); [y_pred, z(:,2)]], [model.na, model.nb, model.nk]);
+        phi_temp(:, 1) = []; % Remove the first column 
         phi_temp = phi_temp(1:length(phi),:);
-        phi(:,1:horizon-2) = phi_temp(:,1:horizon-2);
+        if k <= model.na
+            phi(:,1:k-1) = phi_temp(:,1:k-1);
+        else
+            phi(:,1:model.na) = phi_temp(:,1:model.na);
+        end
         y_pred = phi * model.theta;
 
     end
+
+
 
 end
---
+++ Project 2/My_functions/oefit.m
@@ -0,0 +1,27 @@
+function oe_model = oefit(z, n)
+    nb = n(1);  % Number of B coefficients
+    nf = n(2);  % Number of F coefficients
+    nk = n(3);  % Input delay
+
+    % Estimate a high-order ARX model
+    high_order = [4*nf, 4*nb, nk];
+    arx_model = arxfit(z, high_order);
+%     arx_model.na = nf;
+%     arx_model.nb = nb;
+
+
+    % Simulate the ARX model output
+    u = z(:, 2);  % input data
+    ys = idsimulate(arx_model, u);
+
+    % Estimate the OE model using the original model order
+    oe_model = arxfit([ys, u], [nf, nb, nk]);  % Fit as ARX using output from ARX simulation
+    oe_model.type = 'OE';  
+    oe_model.nf = nf;
+    oe_model.nb = nb;
+    oe_model.nk = nk;
+    
+    oe_model.B = oe_model.theta(1:nb)';
+    oe_model.F = oe_model.theta(nb+1:nb+nk);
+
+end
