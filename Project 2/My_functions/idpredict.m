dae41e9189bcda1f9440bbb26c5c8eb9cefdd204
Your Name <you@example.com>
2024-05-07 18:26:31 +0200

    Idpredict for OE aswell

--- Project 2/My_functions/idpredict.m
+++ Project 2/My_functions/idpredict.m
@@ -1,14 +1,35 @@
 function y_pred = idpredict(model, z, horizon)
 
-    zero = max(model.na, model.nb + model.nk - 1); % Extend model order for zero padding for t < 0
-    phi = uy2phi([zeros(zero, 2); z], [model.na, model.nb, model.nk]); % Generate regressor
-    phi(:, 1) = []; % Remove the first column 
-    y_pred = phi * model.theta; % 1 step prediction
 
-    for k = 2:horizon
+    if isequals(modell.type,'ARX')
 
-        phi = [[zeros(k,model.na); y_pred(k:end-1), phi(k:length(phi)-1,1:model.na-1)], [phi(:,model.na+1:end)]];
+        zero = max(model.na, model.nb + model.nk - 1); % Extend model order for zero padding for t < 0
+        phi = uy2phi([zeros(zero, 2); z], [model.na, model.nb, model.nk]); % Generate regressor
+        phi(:, 1) = []; % Remove the first column
         y_pred = phi * model.theta; % 1 step prediction
+
+        for k = 2:horizon
+
+            phi = [[zeros(k,model.na); y_pred(k:end-1), phi(k:length(phi)-1,1:model.na-1)], [phi(:,model.na+1:end)]];
+            y_pred = phi * model.theta; % 1 step prediction
+
+        end
+
+    end
+
+    if isequals(modell.type,'OE')
+
+        zero = max(model.nf, model.nb + model.nk - 1); % Extend model order for zero padding for t < 0
+        phi = uy2phi([zeros(zero, 2); z], [model.na, model.nb, model.nk]); % Generate regressor
+        phi(:, 1) = []; % Remove the first column
+        y_pred = phi * model.theta; % 1 step prediction
+
+        for k = 2:horizon
+
+            phi = [[zeros(k,model.nf); y_pred(k:end-1), phi(k:length(phi)-1,1:model.na-1)], [phi(:,model.na+1:end)]];
+            y_pred = phi * model.theta; % 1 step prediction
+
+        end
 
     end
 
