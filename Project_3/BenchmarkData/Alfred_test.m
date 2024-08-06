89792d89c223df32ea51032909395477bf72dff3
AronssonAlfred <152867194+AronssonAlfred@users.noreply.github.com>
2024-08-06 20:08:01 +0200

    bunch of stuff

--- Project_3/BenchmarkData/Alfred_test.m
+++ Project_3/BenchmarkData/Alfred_test.m
@@ -11,26 +11,26 @@ VALI2 = load('F16Data_FullMSine_Level4_Validation.mat');
 ESTIM3 = load('F16Data_FullMSine_Level7.mat'); 
 VALI3 = load('F16Data_FullMSine_Level6_Validation.mat');
 
-%% Consider SISO-case with force as input and acceleration at node 1 as output
+%% Consider SISO-case with force as input and acceleration at mode 1 as output
 
 % Estimation data
 output_ESTIM1 = ESTIM1.Acceleration(1,:)';
 input_ESTIM1 = ESTIM1.Voltage';
 
 output_ESTIM2 = ESTIM2.Acceleration(1,:)';
-input_ESTIM2 = [ESTIM2.Force', ESTIM2.Voltage'];
+input_ESTIM2 = ESTIM2.Voltage';
 
 output_ESTIM3 = ESTIM3.Acceleration(1,:)';
-input_ESTIM3 = [ESTIM3.Force', ESTIM3.Voltage'];
+input_ESTIM3 = ESTIM3.Voltage';
 
 % Validation data
 output_VALI1 = VALI1.Acceleration(1,:)';
 input_VALI1 = VALI1.Voltage';
 
 output_VALI2 = VALI2.Acceleration(1,:)';
-input_VALI2 = [VALI2.Force', VALI2.Voltage'];
+input_VALI2 = VALI2.Voltage';
 
 output_VALI3 = VALI3.Acceleration(1,:)';
-input_VALI3 = [VALI3.Force', VALI3.Voltage'];
+input_VALI3 = VALI3.Voltage';
 
 systemIdentification
